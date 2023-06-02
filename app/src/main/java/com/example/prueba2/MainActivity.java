42dc6eaac07ae3b1e7b5a1b059bf091ea2c476e2
Victor Rodriguez <awtgerry@gmail.com>
2023-06-02 12:34:51 -0600

    Users auth

--- app/src/main/java/com/example/prueba2/MainActivity.java
+++ app/src/main/java/com/example/prueba2/MainActivity.java
@@ -5,13 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText uname, passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uname = (EditText) findViewById(R.id.txt_user);
+        passwd = (EditText) findViewById(R.id.txt_password);
+    }
+
+    public void login(View view) {
+        String input = String.valueOf(uname.getText());
+        if (input.equals("mesa")) {
+            Intent intent = new Intent(this, registroActivity.class);
+            startActivity(intent);
+        }
+        else if ((input.equals("admin") || input.equals("empleado")) && passwd.getText().toString().equals("123")) {
+            Toast.makeText(this, "admin", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void irRegistro(View view){
