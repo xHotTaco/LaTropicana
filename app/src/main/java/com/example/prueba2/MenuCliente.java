e95bcd5f3f1e70781c8fe4cbcac4bfb76e8da75d
Victor Rodriguez <awtgerry@gmail.com>
2023-06-16 14:38:49 -0600

    cliente logout

--- app/src/main/java/com/example/prueba2/MenuCliente.java
+++ app/src/main/java/com/example/prueba2/MenuCliente.java
@@ -1,11 +1,16 @@
 package com.example.prueba2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.prueba2.databinding.ActivityMenuClienteBinding;
 import com.example.prueba2.ui.fragments.CarritoFragment;
@@ -34,6 +39,8 @@ public class MenuCliente extends AppCompatActivity {
                 case R.id.carrito:
                     viewFragment(new CarritoFragment());
                     break;
+                case R.id.salir_cliente:
+                    break;
             }
             return true;
         });
@@ -44,5 +51,16 @@ public class MenuCliente extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    public void logout_cliente(MenuItem item) {
+        SharedPreferences preferences = getSharedPreferences("user.dat", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+        Intent client_exit = new Intent(MenuCliente.this, LoginActivity.class);
+        client_exit.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(client_exit);
+        finish();
     }
 }
