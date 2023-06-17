85239f79a141c08b1d595023522b9aab398360d7
Victor Rodriguez <awtgerry@gmail.com>
2023-06-16 20:05:40 -0600

    Products cards added
    TODO List:
        - Falta agregar productos al carrito.
        - Si se compra en el carrito agregar a la cuenta.
        - Mandar pedidos a los empleados.
        - Usar listas para almacenar productos.

---
+++ app/src/main/java/com/example/prueba2/ui/adaptadores/RecyclerAdapter.java
@@ -0,0 +1,79 @@
+package com.example.prueba2.ui.adaptadores;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.prueba2.R;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mSizes = new ArrayList<>();
+    private ArrayList<String> mPrices = new ArrayList<>();
+    private Context context;
+
+    public RecyclerAdapter(Context context, ArrayList<String> mImageNames, ArrayList<String> mNames, ArrayList<String> mSizes, ArrayList<String> mPrices) {
+        this.mImageNames = mImageNames;
+        this.mNames = mNames;
+        this.mSizes = mSizes;
+        this.mPrices = mPrices;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) { // data
+
+        Glide.with(context)
+                .asBitmap()
+                .load(mImageNames.get(position))
+                .into(holder.image);
+
+        holder.name.setText(mNames.get(position));
+        holder.size.setText(mSizes.get(position));
+        holder.price.setText(mPrices.get(position));
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, mNames.get(position) + " agregado al carrito.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView name, size, price;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_product);
+            name = itemView.findViewById(R.id.txt_nombre);
+            size = itemView.findViewById(R.id.txt_tam);
+            price = itemView.findViewById(R.id.txt_precio);
+        }
+    }
+}
--- app/src/main/java/com/example/prueba2/ui/fragments/MenuFragment.java
+++ app/src/main/java/com/example/prueba2/ui/fragments/MenuFragment.java
@@ -3,12 +3,17 @@ package com.example.prueba2.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.prueba2.R;
+import com.example.prueba2.ui.adaptadores.RecyclerAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -48,6 +53,22 @@ public class MenuFragment extends Fragment {
         return fragment;
     }
 
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mSizes = new ArrayList<>();
+    private ArrayList<String> mPrices = new ArrayList<>();
+
+    private ArrayList<String> mImageNamesWine = new ArrayList<>();
+    private ArrayList<String> mNamesWine = new ArrayList<>();
+    private ArrayList<String> mSizesWine = new ArrayList<>();
+    private ArrayList<String> mPricesWine = new ArrayList<>();
+
+    private ArrayList<String> mImageNamesFood = new ArrayList<>();
+    private ArrayList<String> mNamesFood = new ArrayList<>();
+    private ArrayList<String> mSizesFood = new ArrayList<>();
+    private ArrayList<String> mPricesFood = new ArrayList<>();
+    private View viewRoot;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +82,69 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        viewRoot = inflater.inflate(R.layout.fragment_menu, container, false);
+        createBeerView();
+        createWineView();
+        createFoodView();
+
+        return viewRoot;
+    }
+
+    private void createBeerView() {
+        mImageNames.add("https://hebmx.vtexassets.com/arquivos/ids/604983-800-800?v=638218521196070000&width=800&height=800&aspect=true");
+        mNames.add("Corona extra");
+        mSizes.add("355 ml");
+        mPrices.add("$21.00");
+
+        mImageNames.add("https://lapencavinos.com/wp-content/uploads/2019/10/modelo-especial.png");
+        mNames.add("Modelo especial");
+        mSizes.add("355 ml");
+        mPrices.add("$28.00");
+
+        mImageNames.add("https://hebmx.vtexassets.com/arquivos/ids/604983-800-800?v=638218521196070000&width=800&height=800&aspect=true");
+        mNames.add("Corona extra");
+        mSizes.add("355 ml");
+        mPrices.add("$21.00");
+
+        mImageNames.add("https://lapencavinos.com/wp-content/uploads/2019/10/modelo-especial.png");
+        mNames.add("Modelo especial");
+        mSizes.add("355 ml");
+        mPrices.add("$28.00");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(viewRoot.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = viewRoot.findViewById(R.id.recycler_view_beers);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerAdapter adapter = new RecyclerAdapter(viewRoot.getContext(), mImageNames, mNames, mSizes, mPrices);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void createWineView() {
+        mImageNamesWine.add("https://www.lanaval.com.mx/107740-product_default/whisky-black-and-white-700-ml.jpg");
+        mNamesWine.add("Black & white");
+        mSizesWine.add("700 ml");
+        mPricesWine.add("$199.00");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(viewRoot.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerViewWine = viewRoot.findViewById(R.id.recycler_view_wine);
+        recyclerViewWine.setLayoutManager(layoutManager);
+        RecyclerAdapter adapterWine = new RecyclerAdapter(viewRoot.getContext(), mImageNamesWine, mNamesWine, mSizesWine, mPricesWine);
+        recyclerViewWine.setAdapter(adapterWine);
+    }
+
+    private void createFoodView() {
+        mImageNamesFood.add("https://static.vecteezy.com/system/resources/previews/021/952/562/original/tasty-hamburger-on-transparent-background-png.png");
+        mNamesFood.add("Hamburguesa");
+        mSizesFood.add("Clasica");
+        mPricesFood.add("$79.00");
+        mImageNamesFood.add("https://www.ballparkbrand.com/sites/default/files/Hero_Dog_Full_Crop_0.png");
+        mNamesFood.add("Hot dog");
+        mSizesFood.add("Clasico con pepinillos");
+        mPricesFood.add("$30.00");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(viewRoot.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerViewFood = viewRoot.findViewById(R.id.recycler_view_food);
+        recyclerViewFood.setLayoutManager(layoutManager);
+        RecyclerAdapter adapterFood = new RecyclerAdapter(viewRoot.getContext(), mImageNamesFood, mNamesFood, mSizesFood, mPricesFood);
+        recyclerViewFood.setAdapter(adapterFood);
     }
 }
