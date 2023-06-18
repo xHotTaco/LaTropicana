fc09e3de4a5544a3abd023ea4150e656a1d888e9
Victor Rodriguez <awtgerry@gmail.com>
2023-06-18 11:11:25 -0600

    Version final

--- app/src/main/java/com/example/prueba2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/prueba2/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.prueba2.ui.home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,22 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.prueba2.Producto;
 import com.example.prueba2.databinding.FragmentHomeBinding;
+import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Type;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private TextView txt_pedidos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +33,27 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        txt_pedidos = (TextView) binding.txtPedidosRes;
+
+        SharedPreferences pedidos = getActivity().getSharedPreferences("pedido", getActivity().MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = pedidos.getString("arr_productos", null);
+        Type type = new com.google.gson.reflect.TypeToken<Producto[]>(){}.getType();
+        Producto[] productos = gson.fromJson(json, type);
+        if (productos == null) {
+            txt_pedidos.setText("No hay productos en el carrito");
+        }
+        else {
+            String resultado = "";
+            for (int i = 0; i < productos.length; i++) {
+                resultado += "Producto: " + productos[i].getNombre() + "\n";
+                resultado += "Precio: " + productos[i].getPrecio() + "\n";
+                resultado += "Detalles: " + productos[i].getDescripcion() + "\n";
+                resultado += "--------------------------\n";
+            }
+            txt_pedidos.setText(resultado);
+        }
 
         //final TextView textView = binding.textHome;
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
